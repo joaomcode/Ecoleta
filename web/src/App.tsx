df3c80b886902b7d6b123daa6894d30978c2e7c8
joaomcode <joaomarcelo.co@hotmail.com>
2020-06-06 01:20:36 -0300

    Conclusão web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes'
 
 function App() {
-  // useState retorna um array -> [valor do estado, função para atualizar o valor do estado]
-  const [ counter, setCounter ] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="Hi There!"/>
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <Routes />
   );
 }
 
